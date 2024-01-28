313d3d98ef06cac6a97eb5fa0735e13b317edfef
YADAV Manjunatha <manjunathyadavs1998@gmail.com>
2024-01-28 17:54:08 +0530

    pushing JUnit to remote repo

---
+++ Junit/src/test/java/com/example/spring/Junit/JunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.Junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
