0db8f97f14be53e6bf634216773c52f2f0ea7558
MSA63 <manjunathyadavs1998@gmail.com>
2024-01-29 20:06:49 +0530

    first commit

---
+++ Spring1/java/src/test/java/com/dassult/spring1/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dassult.spring1.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
